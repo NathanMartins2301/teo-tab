7e005a5b04c7fe88433e128e12170ff82e2ebaee
NathanMartins2301 <nathan.carneiro23011@gmail.com>
2024-05-20 13:52:28 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,35 @@
 import database from 'infra/database'
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 AS SUM;')
-  console.log(result)
-  res.status(200).json({ chave: 'Nathan aqui' })
+  const updatedAt = new Date().toISOString()
+  const databaseVersionResult = await database.query('SHOW server_version;')
+  const dataBaseVersionValue = databaseVersionResult.rows[0].server_version
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    'SHOW max_connections;'
+  )
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections
+
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResult = await database.query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;',
+    values: [databaseName],
+  })
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count
+
+  console.log(databaseOpenedConnectionsValue)
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: 1,
+      },
+    },
+  })
 }
